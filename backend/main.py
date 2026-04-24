711f564beaea854f4481546ed90cb08e11d0b89f
ganeshv9361-droid <ganeshv9361@gmail.com>
2026-04-24 10:46:16 +0530

    update CORS for vercel

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,10 @@ app = FastAPI(title="Learnly LMS API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://learnly-lms.vercel.app",
+    "http://localhost:5173",
+    "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
