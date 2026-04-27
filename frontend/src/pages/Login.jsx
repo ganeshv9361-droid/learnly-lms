7d29d7221d6df568664ce6f913ae3a3b1af22355
ganeshv9361-droid <ganeshv9361@gmail.com>
2026-04-27 21:23:46 +0530

    Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -78,7 +78,7 @@ export default function Login({ onSwitch }) {
           </div>
 
           {/* Gradient title */}
-          <h1 className="text-4xl font-display gradient-text">
+          <h1 className="flex justify-centre font-display gradient-text">
             Learnly
           </h1>
 
